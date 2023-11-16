be2e527b88af463220976f6cab7ee5a1068747fd
SoufianeAbo <abounasrsoufiane@gmail.com>
2023-11-16 10:25:23 +0100

    change to procedural

--- index.php
+++ index.php
@@ -4,133 +4,129 @@ $username = "root";
 $password = "";
 $database = "dataware";
 
-$sql = new mysqli($servername, $username, $password, $database);
-$table = "SELECT * from teams";
-$result = $sql->query($table);
+$sql = mysqli_connect($servername, $username, $password, $database);
 
-if ($sql->connect_error) {
-    die("Connection failed: " . $sql->connect_error);
+if (!$sql) {
+    die("Connection failed: " . mysqli_connect_error());
 } else {
+    echo '<div class="text-center">';
+    echo '<h1 class="text-xl font-bold border-b-2 border-black w-fit m-auto mb-5">Your data:</h1>';
     
-    echo '<div class = "text-center">';
-    echo '<h1 class = "text-xl font-bold border-b-2 border-black w-fit m-auto mb-5">Your data:</h1>';
-    // echo '<div class = "border w-fit m-auto border-black p-4">';
-    echo '<table class = "m-auto border border-black" id = "teamsTable">';
+    // Teams Table
+    $table = "SELECT * FROM teams";
+    $result = mysqli_query($sql, $table);
+
+    echo '<table class="m-auto border border-black" id="teamsTable">';
     echo '<thead>';
     echo '<tr>
-            <th class = "p-4 border-black border bg-gray-300">ID</th>
-            <th class = "p-4 border-black border bg-gray-300">Name</th>
-            <th class = "p-4 border-black border bg-gray-300">Date of Creation</th>
+            <th class="p-4 border-black border bg-gray-300">ID</th>
+            <th class="p-4 border-black border bg-gray-300">Name</th>
+            <th class="p-4 border-black border bg-gray-300">Date of Creation</th>
         </tr>';
     echo '</thead>';
     echo '<tbody class>';
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            // echo '<p class = "border border-black p-2">';
-            echo '<tr class = "text-center">';
-            echo "<td class = 'border border-black p-2'>{$row['id']}</td><td class = 'border border-black p-2'>{$row['name']}</td><td class = 'border border-black p-2'>{$row['dateCreation']}</td>";
-            // echo "</p>";
+    
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<tr class="text-center">';
+            echo "<td class='border border-black p-2'>{$row['id']}</td><td class='border border-black p-2'>{$row['name']}</td><td class='border border-black p-2'>{$row['dateCreation']}</td>";
             echo '</tr>';
         }
     } else {
         echo "0 results";
     }
-
-    // echo "</div>";
+    
+    echo '</tbody>';
     echo '</table>';
-    echo "</div>";
-
+    
+    // Joined Table
     $table = "SELECT employee.id, employee.name AS employee_name, employee.lastName, employee.email, employee.phone, 
-    employee.role, employee.team, employee.status, 
-    teams.name AS team_name, teams.dateCreation
-    FROM employee
-    INNER JOIN teams ON employee.team=teams.id";
+                employee.role, employee.team, employee.status, 
+                teams.name AS team_name, teams.dateCreation
+                FROM employee
+                INNER JOIN teams ON employee.team=teams.id";
+    $result = mysqli_query($sql, $table);
 
-    $result = $sql->query($table);
-    echo '<table class = "m-auto border border-black hidden" id = "joinedTable">';
+    echo '<table class="m-auto border border-black hidden" id="joinedTable">';
     echo '<thead>';
     echo '<tr>
-            <th class = "p-4 border-black border bg-gray-300">ID</th>
-            <th class = "p-4 border-black border bg-gray-300">First Name</th>
-            <th class = "p-4 border-black border bg-gray-300">Last Name</th>
-            <th class = "p-4 border-black border bg-gray-300">E-Mail</th>
-            <th class = "p-4 border-black border bg-gray-300">Phone Number</th>
-            <th class = "p-4 border-black border bg-gray-300">Role</th>
-            <th class = "p-4 border-black border bg-gray-300">Team</th>
-            <th class = "p-4 border-black border bg-gray-300">Status</th>
-            <th class = "p-4 border-black border bg-gray-300">Name</th>
-            <th class = "p-4 border-black border bg-gray-300">Date Creation</th>
+            <th class="p-4 border-black border bg-gray-300">ID</th>
+            <th class="p-4 border-black border bg-gray-300">First Name</th>
+            <th class="p-4 border-black border bg-gray-300">Last Name</th>
+            <th class="p-4 border-black border bg-gray-300">E-Mail</th>
+            <th class="p-4 border-black border bg-gray-300">Phone Number</th>
+            <th class="p-4 border-black border bg-gray-300">Role</th>
+            <th class="p-4 border-black border bg-gray-300">Team</th>
+            <th class="p-4 border-black border bg-gray-300">Status</th>
+            <th class="p-4 border-black border bg-gray-300">Name</th>
+            <th class="p-4 border-black border bg-gray-300">Date Creation</th>
         </tr>';
     echo '</thead>';
     echo '<tbody class>';
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            // echo '<p class = "border border-black p-2">';
-            echo '<tr class = "text-center">';
-            echo "<td class = 'border border-black p-2'>{$row['id']}</td>
-            <td class = 'border border-black p-2'>{$row['employee_name']}</td>
-            <td class = 'border border-black p-2'>{$row['lastName']}</td>
-            <td class = 'border border-black p-2'>{$row['email']}</td>
-            <td class = 'border border-black p-2'>{$row['phone']}</td>
-            <td class = 'border border-black p-2'>{$row['role']}</td>
-            <td class = 'border border-black p-2'>{$row['team']}</td>
-            <td class = 'border border-black p-2'>{$row['status']}</td>
-            <td class = 'border border-black p-2'>{$row['team_name']}</td>
-            <td class = 'border border-black p-2'>{$row['dateCreation']}</td>";
-            // echo "</p>";
-            echo '</tr>';
-        }
-    } else {
-        echo "0 results";
-    }
-    // echo "</div>";
-    echo '</table>';
-    echo "</div>";
-
     
-    // echo "</div>";
-    echo '</table>';
-    echo "</div>";
-
-    $table = "SELECT * FROM employee";
-
-    $result = $sql->query($table);
-    echo '<table class = "m-auto border border-black hidden" id = "employeesTable">';
-    echo '<thead>';
-    echo '<tr>
-            <th class = "p-4 border-black border bg-gray-300">ID</th>
-            <th class = "p-4 border-black border bg-gray-300">First Name</th>
-            <th class = "p-4 border-black border bg-gray-300">Last Name</th>
-            <th class = "p-4 border-black border bg-gray-300">E-Mail</th>
-            <th class = "p-4 border-black border bg-gray-300">Phone Number</th>
-            <th class = "p-4 border-black border bg-gray-300">Role</th>
-            <th class = "p-4 border-black border bg-gray-300">Team</th>
-            <th class = "p-4 border-black border bg-gray-300">Status</th>
-        </tr>';
-    echo '</thead>';
-    echo '<tbody class>';
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            // echo '<p class = "border border-black p-2">';
-            echo '<tr class = "text-center">';
-            echo "<td class = 'border border-black p-2'>{$row['id']}</td>
-            <td class = 'border border-black p-2'>{$row['name']}</td>
-            <td class = 'border border-black p-2'>{$row['lastName']}</td>
-            <td class = 'border border-black p-2'>{$row['email']}</td>
-            <td class = 'border border-black p-2'>{$row['phone']}</td>
-            <td class = 'border border-black p-2'>{$row['role']}</td>
-            <td class = 'border border-black p-2'>{$row['team']}</td>
-            <td class = 'border border-black p-2'>{$row['status']}</td>";
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<tr class="text-center">';
+            echo "<td class='border border-black p-2'>{$row['id']}</td>
+            <td class='border border-black p-2'>{$row['employee_name']}</td>
+            <td class='border border-black p-2'>{$row['lastName']}</td>
+            <td class='border border-black p-2'>{$row['email']}</td>
+            <td class='border border-black p-2'>{$row['phone']}</td>
+            <td class='border border-black p-2'>{$row['role']}</td>
+            <td class='border border-black p-2'>{$row['team']}</td>
+            <td class='border border-black p-2'>{$row['status']}</td>
+            <td class='border border-black p-2'>{$row['team_name']}</td>
+            <td class='border border-black p-2'>{$row['dateCreation']}</td>";
             echo '</tr>';
         }
     } else {
         echo "0 results";
     }
-    // echo "</div>";
+
+    echo '</tbody>';
     echo '</table>';
-    echo "</div>";
+    
+    // Employees Table
+    $table = "SELECT * FROM employee";
+    $result = mysqli_query($sql, $table);
+
+    echo '<table class="m-auto border border-black hidden" id="employeesTable">';
+    echo '<thead>';
+    echo '<tr>
+            <th class="p-4 border-black border bg-gray-300">ID</th>
+            <th class="p-4 border-black border bg-gray-300">First Name</th>
+            <th class="p-4 border-black border bg-gray-300">Last Name</th>
+            <th class="p-4 border-black border bg-gray-300">E-Mail</th>
+            <th class="p-4 border-black border bg-gray-300">Phone Number</th>
+            <th class="p-4 border-black border bg-gray-300">Role</th>
+            <th class="p-4 border-black border bg-gray-300">Team</th>
+            <th class="p-4 border-black border bg-gray-300">Status</th>
+        </tr>';
+    echo '</thead>';
+    echo '<tbody class>';
+    
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<tr class="text-center">';
+            echo "<td class='border border-black p-2'>{$row['id']}</td>
+            <td class='border border-black p-2'>{$row['name']}</td>
+            <td class='border border-black p-2'>{$row['lastName']}</td>
+            <td class='border border-black p-2'>{$row['email']}</td>
+            <td class='border border-black p-2'>{$row['phone']}</td>
+            <td class='border border-black p-2'>{$row['role']}</td>
+            <td class='border border-black p-2'>{$row['team']}</td>
+            <td class='border border-black p-2'>{$row['status']}</td>";
+            echo '</tr>';
+        }
+    } else {
+        echo "0 results";
+    }
+
+    echo '</tbody>';
+    echo '</table>';
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
