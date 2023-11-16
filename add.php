5332a4bba57f6bb1fa687ff3698ef1559f5fdd15
SoufianeAbo <abounasrsoufiane@gmail.com>
2023-11-16 11:24:16 +0100

    add add page

---
+++ add.php
@@ -0,0 +1,94 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "dataware";
+
+$sql = mysqli_connect($servername, $username, $password, $database);
+
+if (!$sql) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST["name"];
+        $lastName = $_POST["lastName"];
+        $email = $_POST["email"];
+        $phone = $_POST["phone"];
+        $role = $_POST["role"];
+        $team = $_POST["team"];
+        $status = $_POST["status"];
+
+        $name = mysqli_real_escape_string($sql, $name);
+        $lastName = mysqli_real_escape_string($sql, $lastName);
+        $email = mysqli_real_escape_string($sql, $email);
+        $phone = mysqli_real_escape_string($sql, $phone);
+        $role = mysqli_real_escape_string($sql, $role);
+        $team = mysqli_real_escape_string($sql, $team);
+        $status = mysqli_real_escape_string($sql, $status);
+
+        $insert = "INSERT INTO employee (name, lastName, email, phone, role, team, status)
+                    VALUES('$name', '$lastName', '$email', '$phone', '$role', '$team', '$status')";
+        
+        if (mysqli_query($sql, $insert)) {
+            header("Location: index.php?success=1");
+            exit();
+        } else {
+            echo "Error: " . $insert . "<br>" . mysqli_error($sql);
+        }
+
+        mysqli_close($sql);
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class = "text-center mt-24 border border-black rounded bg-gray-200 w-fit m-auto p-8">
+        <h1 class = "text-xl font-bold">Enter your information</h1>
+
+        <form action = "add.php" method = "post">
+            <label for = "name">First Name:</label>
+            <input class = "border border-black mt-2" type = "text" id = "name" name = "name" required>
+            <br>
+
+            <label for = "lastName">Last Name:</label>
+            <input class = "border border-black mt-2" type = "text" id = "lastName" name = "lastName" required>
+            <br>
+
+            <label for = "email">E-Mail:</label>
+            <input class = "border border-black mt-2" type = "email" id = "email" name = "email" required>
+            <br>
+
+            <label for = "phone">Phone Number:</label>
+            <input class = "border border-black mt-2" type = "text" id = "phone" name = "phone" required>
+            <br>
+
+            <label for = "role">Role:</label>
+            <input class = "border border-black mt-2" type = "text" id = "role" name = "role" required>
+            <br>
+
+            <label for = "team">Team:</label>
+            <input class = "border border-black mt-2" type = "number" id = "team" name = "team" required>
+            <br>
+
+            <label for = "status">Status:</label>
+            <input class = "border border-black mt-2" type = "text" id = "status" name = "status" required>
+            <br>
+
+            <button type = "submit" value = "Submit" class="bg-green-500 hover:bg-green-700 text-white py-2 px-4 border border-green-700 rounded-full transition-all mt-4 px-8" id = "personalBtn">
+                Add
+            </button>
+        </form>
+    </div>
+</body>
+</html>
--- commands.sql
+++ commands.sql
@@ -25,12 +25,21 @@ CREATE TABLE IF NOT EXISTS employee(
                                 status varchar(50)
                                 );
 
+-- show the tables
 SELECT * FROM teams;
 SELECT * FROM employee;
 
+-- this allows adding data to the employee table by using INSERT INTO VALUES()
+-- id isn't here because it automatically increments
 INSERT INTO employee(name, lastName, email, phone, role, team, status)
 VALUES("Zouheir", "Rhoumri", "zouheirrhoumri@gmail.com", "+212 61 129 9210", "Directeur des opérations", 1, "Congé");
 
-SELECT employee.id, employee.name, employee.lastName, employee.email, employee.phone, employee.role, employee.team, employee.status, teams.name, teams.dateCreation
+-- this allows adding data to the teams table
+INSERT INTO teams(name, dateCreation)
+VALUES("NightCrawler", "2023-09-20");
+
+-- this is the joining between employees and teams
+-- also adding alias to employee and teams because they have the same name
+SELECT employee.id, employee.name AS employee_name, employee.lastName, employee.email, employee.phone, employee.role, employee.team, employee.status, teams.name AS teams_name, teams.dateCreation
 FROM employee
 INNER JOIN teams ON employee.team=teams.id;
